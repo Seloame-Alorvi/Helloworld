de99b416df24578a721756900cee86dd36b2a4c8
Seloame_Alorvi <seloame.alorvi@student.kpu.ca>
2023-10-31 14:40:37 -0700

    It begins

---
+++ sugur_blob.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+   pinmode(16,OUTPUT);
+   pinMode(18,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // THe code begins . 
+  
+}
